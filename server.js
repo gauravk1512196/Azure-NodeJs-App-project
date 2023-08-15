c704a957a9b0d7652f980d18f4ef38ef3b5003d8
gauravk1512196 <78565082+gauravk1512196@users.noreply.github.com>
2023-08-15 20:41:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:8081"
+  origin: "http://gaurav-mp01-dbserver.mysql.database.azure.com"
 };
 
 app.use(cors(corsOptions));
